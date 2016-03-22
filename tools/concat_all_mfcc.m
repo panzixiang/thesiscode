73d6c3b829a952ef5fa535a9dde066a963e551d7
Sean Z.X. Pan <zpan@spin.CS.Princeton.EDU>
2016-03-21 22:36:08 -0400

    removed copy of files in original dir

--- tools/concat_all_mfcc.m
+++
@@ -1,9 +0,0 @@
-allsongcat = [];
-for i = 1:length(allmfcc)
-    songmfcc = cell2mat(allmfcc(i));
-    songcat = [];
-    for j = 1:1000
-        songcat = vertcat(songcat, songmfcc(:,j));
-    end
-    allsongcat = horzcat(allsongcat, songcat);   
-end
