40f74ab411dc2c4b5bf9805006b7024d56aca8e8
Sean Z.X. Pan <zpan@spin.CS.Princeton.EDU>
2016-04-15 16:56:25 -0400

    building dictionaries

--- arXiv/bs.py
+++ arXiv/bs.py
@@ -1,21 +1,24 @@
 from bs4 import BeautifulSoup
 
-soup = BeautifulSoup(open("datasets/math.txt"), "html5lib")
+soup = BeautifulSoup(open("datasets/all.xml"), "html5lib")
 #printb"(soup.prettify())
 
 #find all sub-categories
 allcats = [str(x) for x in soup.find_all('categories')]
-#cleanup 
+#cleanup tags 
 allcats = [entry.replace('<categories>', '').replace('</categories>','') for entry in allcats]
-#print allcats[0]
+#split individual tags and get the first as the catgory
+allcatsplit = [taglist.split() for taglist in allcats]
+pri_tagset = [cat[0].split('.', 1)[0]  for cat in allcatsplit]
+#see uniques
+uniq_tagset = sorted(set(pri_tagset))
 
 #list all abstracts
 allabs = [str(x) for x in soup.find_all('abstract')]
-#cleanup
+#cleanup tags
 allabs = [entry.replace('<abstract>', '').replace('</abstract>','').replace('\n',' ').strip() for entry in allabs]
-#print len(allabs)
-#print allabs[0]
 
+#concat for future use
 withtag = map(lambda x,y:x+': '+y, allcats, allabs)
 withtag.sort()
 #for x in withtag[0:2]:
@@ -23,3 +26,9 @@ withtag.sort()
 #    print '\n'
 
 print allabs[1]
+
+
+#build individual lists
+for i in range(len(pri_target)):
+        if 'astro' in pri_target[i]: 
+
