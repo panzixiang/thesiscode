f132a218767cd40be3753c9f05fedc4446561046
Sean <panzixiang@gmail.com>
2016-04-15 17:05:07 -0400

    built dictionaries

--- arXiv/bs.py
+++ arXiv/bs.py
@@ -28,7 +28,58 @@ withtag.sort()
 print allabs[1]
 
 
-#build individual lists
-for i in range(len(pri_target)):
-        if 'astro' in pri_target[i]: 
+#['astro-ph', 'cond-mat', 'cs', 'gr-qc', 'hep-ex', 'hep-lat', 'hep-ph', 'hep-th', 'math', 'math-ph', 'nlin', 'nucl-ex', 'nucl-th', 'physics', 'q-bio', 'q-fin', 'quant-ph', 'stat']
 
+#build individual lists
+astro = []
+cond = []
+cs = []
+hep = []
+math = []
+nlin = []
+nucl = []
+physics = []
+q-bio = []
+q-fin = []
+quant = []
+stat = []
+for i in range(len(pri_target)):
+    if 'astro' in pri_target[i]:
+        astro.append(allabs[i])
+    elif 'cond' in pri_target[i]:
+        cond.append(allabs[i])
+    elif 'cs' in pri_target[i]:
+        cs.append(allabs[i])
+    elif 'hep' in pri_target[i]:
+        hep.append(allabs[i])
+    elif 'math' in pri_target[i]:
+        math.append(allabs[i])
+    elif 'nlin' in pri_target[i]:
+        nlin.append(allabs[i])
+    elif 'nucl' in pri_target[i]:
+        nucl.append(allabs[i])
+    elif 'physics' in pri_target[i]:
+        physics.append(allabs[i])
+    elif 'q-bio' in pri_target[i]:
+        q-bio.append(allabs[i])
+    elif 'q-fin' in pri_target[i]:
+        q-fin.append(allabs[i])
+    elif 'quant' in pri_target[i]:
+        quant.append(allabs[i])
+    elif 'stat' in pri_target[i]:
+        stat.append(allabs[i])
+    else:
+        pass  
+
+print len(astro)
+print len(cond)
+print len(cs)
+print len(hep)
+print len(math)
+print len(nlin)
+print len(nucl)
+print len(physics)
+print len(q-bio)
+print len(q-fin)
+print len(quant)
+print len(stat)
