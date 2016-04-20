69c8e085147cf51a32b9bc42fd9cd67c2dbc28bc
Sean <panzixiang@gmail.com>
2016-04-19 21:25:56 -0400

    being full lda

--- arXiv/fulllda.py
+++ arXiv/fulllda.py
@@ -7,6 +7,7 @@ import pickle
 import csv
 import sys
 import numpy as np
+import scipy as sp
 
 tokenizer = RegexpTokenizer(r'\w+')
 
@@ -29,9 +30,6 @@ doc_set = arxiv_11['math']
 
 # list for tokenized documents in loop
 texts = []
-
-# collate each document as stemmed tokens
-doc_tokens = []
 
 # loop through document list
 for i in doc_set:
@@ -64,4 +62,46 @@ ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=num_topics, id2wor
 # look at topic proportion of one document
 print ldamodel[dictionary.doc2bow(texts[0])]
 
+# build topic proportion matrix
+topicPropArray = np.zeros((len(texts), num_topics))
+for i in range(len(texts)):
+    text = texts[i]
+    textProp = ldamodel[dictionary.doc2bow(text)]
+    for pair in textProp:
+        topicIdx = pair[0]
+        weight = pair[1]
+        topicPropArray[i, topicIdx] = weight
 
+# test on new data
+test_set = arxiv_12['math'][0:9]
+
+# loop through test list
+for i in test_set:
+    
+    # clean and tokenize document string
+    raw = i.lower()
+    tokens = tokenizer.tokenize(raw)
+
+    # remove stop words from tokens
+    stopped_tokens = [i for i in tokens if not i in en_stop]
+    
+    # stem tokens
+    stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+    
+    # add tokens to list
+    test_set.append(stemmed_tokens)
+
+# calculate similarity measure
+confidence = []
+for test in test_set:
+    sim_score = np.zeros(len(texts))
+    test_vec = np.zeros(1, num_topics)
+    newProp = ldamodel[dictionary.doc2bow(test)]
+    for pair in newProp:
+        topicIdx = pair[0]
+        weight = pair[1]
+        test_vec[topicIdx] = weight
+    simscore = [scipy.spatial.distance.jaccard(test_vec, row) for row in topicPropArray]
+    max_score = np.amax(sim_score)
+    print max_score
+    confidence.append(max_score)
