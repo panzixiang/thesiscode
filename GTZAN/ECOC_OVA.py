58ecd27db4eb2714093710a185ce4e926db27d98
Sean <panzixiang@gmail.com>
2016-04-21 03:40:09 -0400

    OVA and OVO for 2 feature

--- GTZAN/ECOC_OVA.py
+++ GTZAN/ECOC_OVA.py
@@ -74,16 +74,16 @@ def main():
     #rf = RandomForestClassifier(n_estimators=50, max_features = 'log2')
     sgd = SGDClassifier(loss="hinge", penalty="l2")
     #svc = svm.SVC(kernel='linear')
-    #dtree = DecisionTreeClassifier(max_depth =3)
+    dtree = DecisionTreeClassifier(max_depth =3)
     lsvc = LinearSVC(random_state=0)
-    cla = OneVsRestClassifier(sgd)
+    cla = OneVsRestClassifier(dtree)
 
     cm_all = np.zeros((10,10), dtype=np.int)
 
     cb = np.zeros((10,20))
     losses = []
    
-    with open('ECOC_sgd_OVA.csv','w') as f1:
+    with open('ECOC_dtree_OVA.csv','w') as f1:
         wrtest = csv.writer(f1, quoting=csv.QUOTE_NONNUMERIC,lineterminator='\n')
         scores = 0.0
         for train, test in kf:
--- GTZAN/ECOC_OVO.py
+++ GTZAN/ECOC_OVO.py
@@ -75,16 +75,16 @@ def main():
     #rf = RandomForestClassifier(n_estimators=50, max_features = 'log2')
     sgd = SGDClassifier(loss="hinge", penalty="l2")
     #svc = svm.SVC(kernel='linear')
-    #dtree = DecisionTreeClassifier(max_depth =3)
+    dtree = DecisionTreeClassifier(max_depth =3)
     lsvc = LinearSVC(random_state=0)
-    cla = OneVsOneClassifier(sgd)
+    cla = OneVsOneClassifier(dtree)
 
     cm_all = np.zeros((10,10), dtype=np.int)
 
     cb = np.zeros((10,20))
     losses = []
    
-    with open('ECOC_sgd_OVO.csv','w') as f1:
+    with open('ECOC_dtree_OVO.csv','w') as f1:
         wrtest = csv.writer(f1, quoting=csv.QUOTE_NONNUMERIC,lineterminator='\n')
         scores = 0.0
         for train, test in kf:
