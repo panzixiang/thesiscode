612cc6186862136e45279528da895fc9a923ea8e
Sean <panzixiang@gmail.com>
2016-04-21 19:27:24 -0400

    split training lda

--- arXiv/fulllda.py
+++ arXiv/fulllda.py
@@ -157,7 +157,7 @@ def tokenize(doc_set):
         # add tokens to list
         doc_texts.append(stemmed_tokens)
 
-return doc_texts   
+    return doc_texts   
 
 if __name__ == "__main__":
     main()     
