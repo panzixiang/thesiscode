7e7f6af7c77a7c470e2744de3da031cfeb571b82
Sean <panzixiang@gmail.com>
2016-04-24 23:44:44 -0400

    modified plotcm

--- arXiv/plotcm.py
+++ arXiv/plotcm.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import zero_one_loss
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
@@ -9,29 +10,32 @@ def main():
                [6]*100 + [7]*100 + [8]*100 + [9]*100 + [10]*100 
 
     filename = sys.argv[1]
-    print filename
+    
 
     with open(filename, 'rU') as f:           
         pred = [rec for rec in csv.reader(f, delimiter=',')]
     pred = sum(pred,[])
     pred = [int(x) for x in pred]    
+    print zero_one_loss(pred, test_label)
     cm = confusion_matrix(test_label, pred, labels =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     np.set_printoptions(precision=2)
-    plt.figure()
+    fig = plt.figure()
+    fig.patch.set_facecolor('white')
     plot_confusion_matrix(cm)
     plt.show()
     #plt.imasve('smv_confusion', cm, cmap=plt.cm.viridis)
 
 def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.gist_heat):
+    hfont = {'fontname':'Helvetica'}
     plt.imshow(cm, interpolation='nearest', cmap=cmap, vmin=0, vmax=100)
-    plt.title(title)
+    plt.title(title, fontsize=20, **hfont)
     plt.colorbar()
     tick_marks = np.arange(10)
-    plt.xticks(tick_marks, ['astro', 'cond', 'cs', 'hep', 'math','physics', 'q-bio', 'q-fin', 'quant', 'stat'], rotation=45)
-    plt.yticks(tick_marks, ['astro', 'cond', 'cs', 'hep', 'math','physics', 'q-bio', 'q-fin', 'quant', 'stat'])
+    plt.xticks(tick_marks, ['astro', 'cond', 'cs', 'hep', 'math','physics', 'q-bio', 'q-fin', 'quant', 'stat'], rotation=45, fontsize=16, **hfont)
+    plt.yticks(tick_marks, ['astro', 'cond', 'cs', 'hep', 'math','physics', 'q-bio', 'q-fin', 'quant', 'stat'], fontsize=16, **hfont)
     plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+    plt.ylabel('True label', fontsize=16, **hfont)
+    plt.xlabel('Predicted label', fontsize=16, **hfont)
 
 if __name__ == "__main__":
     main()     
