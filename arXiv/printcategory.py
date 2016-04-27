3f409498e68f7feb9c86e34a8fd626fcd48ea0e1
Sean <panzixiang@gmail.com>
2016-04-27 15:32:50 -0400

    added print

--- arXiv/printcategory.py
+++ arXiv/printcategory.py
@@ -4,8 +4,13 @@ import sys
 
 def main():
     arxiv_11 = pickle.load(open("2011_big_pop.p", "rb"))
-    filename = sys.argv[1]
-    doc_set = arxiv_11[filename]
+    # filename = sys.argv[1]
+    # doc_set = arxiv_11[filename]
+    doc_set = arxiv_11['astro'] + arxiv_11['cond'] + \
+            arxiv_11['cs'] + arxiv_11['hep'] + \
+            arxiv_11['math'] + arxiv_11['physics'] + \
+            arxiv_11['qbio'] + arxiv_11['qfin'] + \
+            arxiv_11['quant'] + arxiv_11['stat'] 
 
     for i in range(len(doc_set)):
         print str(i) + '\t' + doc_set[i]
