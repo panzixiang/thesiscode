f3bdbd9a607600279754b1f74c6899596f5d3703
Sean <panzixiang@gmail.com>
2016-04-27 13:35:30 -0400

    added print

---
+++ arXiv/printcategory.py
@@ -0,0 +1,15 @@
+import pickle
+import sys
+
+
+def main():
+    arxiv_11 = pickle.load(open("2011_big_pop.p", "rb"))
+    filename = sys.argv[1]
+    doc_set = arxiv_11[filename]
+
+    for article in doc_set:
+        print article
+        print '\n'
+
+if __name__ == "__main__":
+    main()             
